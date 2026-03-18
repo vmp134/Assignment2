efe5f014e4429a362804bb68a6ec49feeedce40e
Victor <vmp134@scarletmail.rutgers.edu>
2026-03-18 19:34:21 -0400

    Setup

--- compare.c
+++ compare.c
@@ -4,6 +4,18 @@
 #include <string.h>
 #include <unistd.h>
 
+struct {   
+    char *name;         //The word itself
+    double freq;        //The frequency of said word
+} word;
+
+struct {
+    char *name;         //The filename
+    word *words;        //The array containing word structs
+    int wordCount;      //Total unique words
+    int allWords;       //Total words 
+} fileData;
+
 
 
 
@@ -11,3 +23,7 @@
 int wfd(char *file) {
     
 } 
+
+int jsd() {
+
+}
