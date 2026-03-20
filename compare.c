5b99cdbc3a89559829637c9a5eca308deaa6ecd0
Victor <vmp134@scarletmail.rutgers.edu>
2026-03-20 10:56:39 -0400

    Ditto

--- compare.c
+++ compare.c
@@ -4,26 +4,48 @@
 #include <string.h>
 #include <unistd.h>
 
-struct {   
-    char *name;         //The word itself
-    double freq;        //The frequency of said word
-} word;
+//Initialization
 
-struct {
-    char *name;         //The filename
-    word *words;        //The array containing word structs
-    int wordCount;      //Total unique words
-    int allWords;       //Total words 
-} fileData;
+    struct word {   
+        char *name;         //The word itself
+        double freq;        //The frequency of said word
+    };
 
+    struct fileData{
+        char *name;         //The filename
+        struct word *words; //The array containing word structs
+        int wordCount;      //Total unique words
+        int allWords;       //Total words 
+    };
 
+//Helper Functions
 
+    //Uses binary search to find word insertion point
+    void insert(struct fileData file, struct word x) {
 
+    }
 
-int wfd(char *file) {
-    
-} 
+    //Creates the fileData struct for one file
+    struct fileData create(char* file) {
+        
+    }
 
-int jsd() {
+    //Calculates Word Frequency Distribution
+    int wfd(struct fileData file) {
+        
+    } 
 
-}
+    //Calculates Jenson-Shannon Distance
+    int jsd(struct fileData file) {
+
+    }
+
+//Main
+
+    int main(int argc, char **argv) {
+        //Run a for loop for all args
+        //Recursively search for text files then create filedata struct for all and yadda yadda
+        //Be sure to skip dotfiles (files that start with ".")
+        //We would have n choose 2 comparisons total
+        //I think the function call itself also counts as an arg iirc so skip that
+    }
